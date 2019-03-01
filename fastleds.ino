6dbcb39754f84d4dc6089c05f96da8dd797c5581
alexis103 <47801318+alexis103@users.noreply.github.com>
2019-03-01 09:30:55 -0500

    Update fastleds.ino

--- fastleds.ino
+++ fastleds.ino
@@ -2,35 +2,37 @@
 #define NUM_LEDS 135
 #define DATA_PIN 6
 
-CGRB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 
 void setup() {
-FastLED.addleds<WS2812B, DATA_PIN, GRB> (leds, NUM_LEDS));
+FastLED.addLeds<WS2812B, DATA_PIN, GRB> (leds, NUM_LEDS));
 }
 
-Void loop() {
-for (int i=0; i=135)
-leds[i]= (CRGB;; Green;)
-leds[(NUM_LEDS-i)]=GRB;;
-FastLED.Show();
-fill_solid(leds, 0, 135, (CRGB(GREEN));
-delay(2000);
-fill_solid(leds, 0, 135, (CRGB(BLACK));
-delay(100);
-fill_solid(leds, 0, 135, (CRGB(GREEN));
-delay(2000);
-fill_solid(leds, 0, 135, (CRGB(BLACK));
-delay(100);
-fill_solid(leds, 0, 67, (CRGB(42,165,42));
-fill_solid(leds, 67, 135, (CRGB(GREEN));
-delay(2000);
-fill_solid(leds, 0, 135, (CRGB(BLACK));
-delay(100);
-fill_solid(leds, 0, 67, (CRGB(GREEN));
-fill_solid(leds, 67, 135, (CRGB(42,165,42));
-delay(2000);
-fill_solid(leds, 0, 135, (CRGB(BLACK));
-delay(100);
-fill_rainbow( &(leds[i]), 1, 135 / GREEN/);
-delay(100)
+void loop() {
+for (int i=0;;i=135){
+  leds[i]= (CRGB;Green;)
+  leds[(NUM_LEDS-i)]=GRB;;
+  FastLED.show();
+  fill_solid(leds[0], 70, CRGB(50,0,200));
+  delay(2000);
+  fill_solid(leds, 0, 135, (CRGB(Black)));
+  delay(100);
+  fill_solid(leds, 0, 135, (CRGB(Green)));
+  delay(2000);
+  fill_solid(leds, 0, 135, (CRGB(Black)));
+  delay(100);
+  fill_solid(leds, 0, 67, (CRGB(42,165,42)));
+  fill_solid(leds, 67, 135, (CRGB(Green)));
+  delay(2000);
+  fill_solid(leds, 0, 135, (CRGB(Black)));
+  delay(100);
+  fill_solid(leds, 0, 67, (CRGB(Green)));
+  fill_solid(leds, 67, 135, (CRGB(42,165,42)));
+  delay(2000);
+  fill_solid(leds, 0, 135, (CRGB(Black)));
+  delay(100);
+  fill_rainbow( &(leds[i]), 1, 135 / Green/);
+  delay(100);
 }
+}
+
